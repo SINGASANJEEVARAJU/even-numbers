b75bd5ab0cff26e61ac34a725750b7ca4f2845aa
SINGA SANJEEVARAJU <sanjeevaraj123@gmail.com>
2024-11-14 22:37:02 +0530

    files are added

---
+++ day1.js
@@ -0,0 +1,50 @@
+
+num=+prompt("enter a number:")
+for(i=-1;!(i==-10-1);i--){
+    console.log(i)
+
+}
+
+number=prompt("Enter the Number")
+for (i=-1;!(i<number);i--){
+  console.log(i)
+}
+
+
+
+number=prompt("Enter the Number")
+for (i=-1;!(i==number-1);i--){
+  console.log(i)
+}
+for(i=-1;i>-10-1;i--){
+    console.log(i)
+}
+num=+prompt("enter a number:");
+for(i=num;!(i>=0);i++){
+    console.log(i)
+}
+for (i=-10;!(i==0);i++){
+    console.log(i)
+}
+for(i=-10;i<0;i++){
+    console.log(i)
+}
+
+num=prompt("entrer a number:");
+sum=0
+for(i=0;i<num.length;i++){
+if(+num[i]%2===0){
+    sum=sum+(+num[i])
+}
+    
+}
+console.log(sum)
+num=prompt("entrer a number:");
+sum=0
+for(i=0;i<num.length;i++){
+if(+num[i]%3===1){
+    sum=sum+(+num[i])
+}
+    
+}
+console.log(sum)
